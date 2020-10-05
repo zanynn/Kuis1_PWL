ed4e79065dfce6c8dc929c6c9b9b43c9fd8bae9e
muhammad Fauzan <zanynngoose@gmail.com>
2020-10-05 18:41:56 +0700

    Kuis 1

---
+++ Kuis_Fauzan/storage/framework/views/fad9eb5a25c93d0b2281a28768b563eb541fe142.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title><?php echo $__env->yieldContent('title'); ?></title>
+    <link href="<?php echo e(asset('img/icon.png')); ?>" rel="shortcut icon"">
+    <!-- web-fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,500' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <!-- off-canvas -->
+    <link href="<?php echo e(asset('css/mobile-menu.css')); ?>" rel="stylesheet">
+    <!-- font-awesome -->
+    <link href="<?php echo e(asset('fonts/font-awesome/css/font-awesome.min.css')); ?>" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+          integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- Style CSS -->
+    <link href="<?php echo e(asset('css/style.css')); ?>" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+  <?php echo $__env->yieldContent('content'); ?>
+ 
+  
+
+</body>
+
+</html>
+<?php /**PATH C:\Kuis\resources\views/layouts/Master.blade.php ENDPATH**/ ?>
